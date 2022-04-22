67c6e75ca0a93c100b68d05c111343fcff49cd06
skieabad <abadskie19@gmail.com>
2022-04-23 04:40:25 +0800

    adding verify email screen

---
+++ lib/presentation/screens/verify_email_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class VerifyEmailScreen extends StatefulWidget {
+  const VerifyEmailScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailScreen> createState() => _VerifyEmailScreenState();
+}
+
+class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Verify your email address'),
+        const Text(
+            'To continue your sign up, please verify this email address by clicking the button below'),
+        TextButton(
+          onPressed: () {},
+          child: const Text('Verify your email'),
+        ),
+      ],
+    );
+  }
+}

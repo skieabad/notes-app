72dc04d89219f4b03ee0f4f9539f11a179336aa7
skieabad <abadskie19@gmail.com>
2022-05-16 23:46:07 +0800

    add a stateful widget for this screen

---
+++ lib/presentation/screens/notes/new_note_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewNoteScreen extends StatefulWidget {
+  const NewNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NewNoteScreen> createState() => _NewNoteScreenState();
+}
+
+class _NewNoteScreenState extends State<NewNoteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

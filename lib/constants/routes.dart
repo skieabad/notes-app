ee98773d1209f3303ceb9b3c61c5696d79d92217
skieabad <abadskie19@gmail.com>
2022-05-16 23:44:16 +0800

    add const routes for new notes screen

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,5 +2,8 @@
 const homeRoute = '/';
 const loginRoute = '/login-screen/';
 const signUpRoute = '/signup-screen/';
-const notesRoute = '/notes-screen/';
 const verifyEmailRoute = '/verify-email-screen/';
+
+// ? Notes folder
+const notesRoute = '/notes-screen/';
+const newNotesRoute = '/new-notes-screen/';

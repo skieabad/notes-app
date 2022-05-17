bcfa4a58c829c317f0fa73ae6f5a5e92925d5e03
skieabad <abadskie19@gmail.com>
2022-05-17 14:56:22 +0800

    adjust the ui

--- lib/presentation/screens/notes/notes_screen.dart
+++ lib/presentation/screens/notes/notes_screen.dart
@@ -20,98 +20,179 @@ class _NotesScreenState extends State<NotesScreen> {
   @override
   void initState() {
     _notesService = NotesService();
-    _notesService.open();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: () =>
-                          Navigator.of(context).pushNamed(newNotesRoute),
-                      icon: const Icon(Icons.add),
-                    ),
-                    PopupMenuButton<MenuAction>(
-                      onSelected: (value) async {
-                        switch (value) {
-                          case MenuAction.logout:
-                            final logout = await showLogOutDialog(context);
-                            if (logout) {
-                              await AuthService.firebase().logoutUser();
-                              'logout'.log();
-                              Navigator.of(context).pushNamedAndRemoveUntil(
-                                  loginRoute, (route) => false);
-                            }
-                            break;
-                          case MenuAction.settings:
-                            break;
-                        }
-                      },
-                      itemBuilder: (context) => <PopupMenuEntry<MenuAction>>[
-                        const PopupMenuItem<MenuAction>(
-                          value: MenuAction.logout,
-                          child: Text('Logout'),
-                        ),
-                        const PopupMenuItem<MenuAction>(
-                          value: MenuAction.settings,
-                          child: Text('Settings'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              FutureBuilder(
-                future: _notesService.getOrCreateUser(email: userEmail),
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.done:
-                      return StreamBuilder(
-                        stream: _notesService.allNotes,
-                        builder: (context, snapshot) {
-                          switch (snapshot.connectionState) {
-                            // waiting is perfect with stream builder
-                            case ConnectionState.waiting:
-                            case ConnectionState.active:
-                              return const Center(
-                                child: Text(
-                                  'Waiting to fetch all notes',
-                                ),
-                              );
-                            default:
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                          }
-                        },
-                      );
-                    default:
-                      return const CircularProgressIndicator();
+      // body: SafeArea(
+      // child: Center(
+      //   child: Column(
+      //     children: [
+      //       Align(
+      //         alignment: Alignment.topRight,
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.end,
+      //           children: [
+      //             IconButton(
+      //               onPressed: () =>
+      //                   Navigator.of(context).pushNamed(newNotesRoute),
+      //               icon: const Icon(Icons.add),
+      //             ),
+      //             PopupMenuButton<MenuAction>(
+      //               onSelected: (value) async {
+      //                 switch (value) {
+      //                   case MenuAction.logout:
+      //                     final logout = await showLogOutDialog(context);
+      //                     if (logout) {
+      //                       await AuthService.firebase().logoutUser();
+      //                       'logout'.log();
+      //                       Navigator.of(context).pushNamedAndRemoveUntil(
+      //                           loginRoute, (route) => false);
+      //                     }
+      //                     break;
+      //                   case MenuAction.settings:
+      //                     break;
+      //                 }
+      //               },
+      //               itemBuilder: (context) => <PopupMenuEntry<MenuAction>>[
+      //                 const PopupMenuItem<MenuAction>(
+      //                   value: MenuAction.logout,
+      //                   child: Text('Logout'),
+      //                 ),
+      //                 const PopupMenuItem<MenuAction>(
+      //                   value: MenuAction.settings,
+      //                   child: Text('Settings'),
+      //                 ),
+      //               ],
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //       const SizedBox(
+      //         height: 20,
+      //       ),
+      //       FutureBuilder(
+      //         future: _notesService.getOrCreateUser(email: userEmail),
+      //         builder: (context, snapshot) {
+      //           switch (snapshot.connectionState) {
+      //             case ConnectionState.done:
+      //               return StreamBuilder(
+      //                 stream: _notesService.allNotes,
+      //                 builder: (context, snapshot) {
+      //                   switch (snapshot.connectionState) {
+      //                     // waiting is perfect with stream builder
+      //                     case ConnectionState.waiting:
+      //                     case ConnectionState.active:
+      //                       if (snapshot.hasData) {
+      //                         final allNotes =
+      //                             snapshot.data as List<DatabaseNotes>;
+      //                         return ListView.builder(
+      //                           itemCount: allNotes.length,
+      //                           itemBuilder: (context, index) {
+      //                             final note = allNotes[index];
+      //                             return ListTile(
+      //                               title: Text(
+      //                                 note.text,
+      //                               ),
+      //                             );
+      //                           },
+      //                         );
+      //                       } else {
+      //                         return const Center(
+      //                           child: CircularProgressIndicator(),
+      //                         );
+      //                       }
+      //                     default:
+      //                       return const Center(
+      //                         child: CircularProgressIndicator(),
+      //                       );
+      //                   }
+      //                 },
+      //               );
+      //             default:
+      //               return const Center(
+      //                 child: CircularProgressIndicator(),
+      //               );
+      //           }
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // ),
+      appBar: AppBar(
+        title: const Text('Your notes'),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).pushNamed(newNotesRoute),
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final logout = await showLogOutDialog(context);
+                  if (logout) {
+                    await AuthService.firebase().logoutUser();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
                   }
-                },
+                  break;
+                case MenuAction.settings:
+                  break;
+              }
+            },
+            itemBuilder: (context) => <PopupMenuEntry<MenuAction>>[
+              const PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text('Logout'),
+              ),
+              const PopupMenuItem<MenuAction>(
+                value: MenuAction.settings,
+                child: Text('Settings'),
               ),
             ],
           ),
-        ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: _notesService.getOrCreateUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return StreamBuilder(
+                stream: _notesService.allNotes,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // waiting is perfect with stream builder
+                    case ConnectionState.waiting:
+                    case ConnectionState.active:
+                      if (snapshot.hasData) {
+                        final allNotes = snapshot.data as List<DatabaseNotes>;
+                        print(allNotes);
+                        return const Center(
+                          child: Text('Here are the notes'),
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    default:
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                  }
+                },
+              );
+            default:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+          }
+        },
       ),
     );
   }

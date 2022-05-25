44e83427901219053808f30b14811d77c6446453
skieabad <abadskie19@gmail.com>
2022-05-26 05:23:48 +0800

    add exception

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -15,3 +15,5 @@ class CouldNotDeleteNote implements Exception {}
 class CouldNotUpdateNote implements Exception {}
 
 class UserAlreadyExists implements Exception {}
+
+class UserShouldBeSetBeforeReadingAllNotes implements Exception {}

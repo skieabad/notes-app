61b89c6f28bb9b9b3f5bc5d12995110b073c13b0
skieabad <abadskie19@gmail.com>
2022-05-26 05:20:40 +0800

    generic function for modal arguments

---
+++ lib/utilities/generics/get_arguments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart' show BuildContext, ModalRoute;
+
+// creating an extension as generic function for getting arguments
+extension GetArgument on BuildContext {
+  T? getArgument<T>() {
+    final modalRoute = ModalRoute.of(this);
+    if (modalRoute != null) {
+      final args = modalRoute.settings.arguments;
+      if (args != null && args is T) {
+        return args as T;
+      }
+    }
+    return null;
+  }
+}
